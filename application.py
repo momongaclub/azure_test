2acdc39d6263a1db115e3c3b7a3d903d975e72e2
momongaclub <momongaclub8@gmail.com>
2021-07-01 10:35:12 +0900

    Update application.py

--- application.py
+++ application.py
@@ -24,4 +24,4 @@ def input_form():
     return render_template("result.html", query=query, response=newsdata)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='18102')
+    app.run(debug=True)
